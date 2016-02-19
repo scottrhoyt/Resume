9acd2f31d076d01132fea8ffe971d42e732d912f
Scott Hoyt <scott@wildonion.io>
2016-02-19 14:31:57 -0800

    Added more work experience.

--- Resume.playground/Contents.swift
+++ Resume.playground/Contents.swift
@@ -48,6 +48,40 @@ let blackEdge = WorkExperience(
     ]
 )
 
+let wildOnion = WorkExperience(
+    companyName: "Wild Onion Labs",
+    title: "Cofounder",
+    startDate: { NSDateComponents(month: 11, year: 2013).date! }(),
+    endDate: { NSDateComponents(month: 10, year: 2015).date! }(),
+    notables: [
+        "Technical and strategic consulting for LA-based startups.",
+        "Built Card & Ink, a self-serve kiosk for buying and customizing high quality greeting card from a curated list of Etsy artists."
+    ]
+)
+
+let entrepreneurImpactDay = WorkExperience(
+    companyName: "Entrepreneur Impact Day",
+    title: "Cofounder",
+    startDate: { NSDateComponents(month: 6, year: 2014).date! }(),
+    endDate: { NSDateComponents(month: 10, year: 2015).date! }(),
+    notables: [
+        "Started an event-based non-profit for bringing together talented startups with good causes.",
+        "Innaugural event connected 400 individuals from prominent LA startups and non-profits.",
+        "11 teams completed day-long projects resulting in a variety of actionable deliverables accross design, marketing, and technology."
+    ]
+)
+
+let diamondLabs = WorkExperience(
+    companyName: "Diamond Labs",
+    title: "Mobile Development Lead",
+    startDate: { NSDateComponents(month: 10, year: 2015).date! }(),
+    endDate: { NSDateComponents(month: 2, year: 2016).date! }(),
+    notables: [
+        "Technical and strategic consulting for LA-based startus.",
+        "Built Card & Ink, a self-serve kiosk for buying and customizing high quality greeting card from a curated list of Etsy artists."
+    ]
+)
+
 //: ## Interests
 let interests = [
     "Flying",
@@ -64,7 +98,9 @@ let resume = Resume(
     sections: [
         ResumeSection(title: "Scott Hoyt", body: bio),
         ResumeSection(title: "Education", body: uiuc),
-        ResumeSection(title: "Work Experience", body: [ctc, blackEdge]),
+        ResumeSection(
+            title: "Work Experience",
+            body: [ctc, blackEdge, wildOnion, entrepreneurImpactDay]),
         ResumeSection(title: "Interests", body: interests)
     ]
 )
